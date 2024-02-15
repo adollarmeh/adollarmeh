762cc5a128b035cbb3bd027fd30dd1561d3ccbc2
adollarmeh <160126112+adollarmeh@users.noreply.github.com>
2024-02-15 23:34:42 +1100

    Update script.js

--- script.js
+++ script.js
@@ -29,28 +29,3 @@ function createCloudsAndSymbols() {
 
 // Modal functionality
 var modal = document.getElementById("payment
-
-
-// Get the modal
-var modal = document.getElementById("paymentOptions");
-
-// Get the button that opens the modal
-var btn = document.getElementById("donateButton");
-
-// Function to open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Function to close the modal
-function closeModal() {
-    modal.style.display = "none";
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
